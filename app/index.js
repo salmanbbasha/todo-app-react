331bd31b7d83bcfa10b56a574cfa9e8d40b31f77
Salman Basha <salmanbbasha@gmail.com>
2023-01-31 16:48:46 +0530

    Starter Code

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
